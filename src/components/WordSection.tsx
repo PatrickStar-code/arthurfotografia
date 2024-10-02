6b215f7ff0d2ad0384ea6983471a4d110490fa24
Patrick Almeida <patrick.almeida06@gmail,com>
2024-10-01 21:46:34 -0300

    tentativa #3

--- src/components/WordSection.tsx
+++ src/components/WordSection.tsx
@@ -19,7 +19,7 @@ export default function WordSection() {
       />
       <VelocityScroll
         text="Casamentos Festas Aniversários Formaturas"
-        default_velocity={1}
+        defaultVelocity={1}
         className="font-display text-center text-4xl font-bold tracking-[-0.02em] text-black drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem]"
       />
     </motion.section>
